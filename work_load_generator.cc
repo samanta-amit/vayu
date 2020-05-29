b6748f203fa9dbbf01b7e3963205ddf17bf51f4b
Amit Samanta <amit.samanta049@gmail.com>
2020-05-29 06:10:51 +0530

    testing

--- work_load_generator.cc
+++ work_load_generator.cc
@@ -8,8 +8,8 @@ int main(int argc, char* argv[]){
     std::vector<rpc_client*> rpc_clients;
     std::string server_addr = "0.0.0.0";
     std::string port = "5005";
-    // two clients in two threads
-    for (int i = 0; i < 2; i++)    
+    // three clients in three threads
+    for (int i = 0; i < 3; i++)    
     {
         rpc_client* cl = new rpc_client(i, server_addr, port);
         rpc_clients.push_back( cl );
